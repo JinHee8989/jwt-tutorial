827caacea42e1023539acde4434a5b6bee013bed
wlsgm <wlsgml8989@gmail.com>
2021-01-25 09:01:43 +0900

    #커밋 테스트

--- src/main/java/me/jinhee/jwttutorial/config/SecuriityConfig.java
+++ src/main/java/me/jinhee/jwttutorial/config/SecuriityConfig.java
@@ -1,6 +1,5 @@
 package me.jinhee.jwttutorial.config;
 
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
